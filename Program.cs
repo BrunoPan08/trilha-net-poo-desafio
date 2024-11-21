7ac89679c3276d1b4a70d93e6833013908141963
BrunoPan08 <brunopan08@gmail.com>
2024-11-21 12:00:47 -0300

    Enviando exercício

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia n = new Nokia("99999-9999", "n1", "12345", 254);
+n.InstalarAplicativo("Whatsapp");
+Iphone i = new Iphone("12345-1234", "10", "12345", 254);
+i.InstalarAplicativo("Instagram");
